1d930191e77c8b452c72dad861321855b9492f75
Daniel Ardiles <danielgardiles@gmail.com>
2021-08-03 22:13:50 -0300

    previa maps

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,47 +1,163 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     SafeAreaView,
-    View,
-    Text,ImageBackground,
     Dimensions,
     StyleSheet,
+    Text,
+    View,
 } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
-
-const Map = () => {
-    return (
-        <SafeAreaView>
-        <ImageBackground
-            style={{ height, paddingLeft: width/4, paddingTop: height/3 }}
-            source={require('../assets/images/Lamp.jpg')}
-        >
-            <View style={styles.box} >
-                <Text style={styles.text} >
-                    Map
-                </Text>
+import { Image, Icon } from 'react-native-elements';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import { hasLocationPermission } from '../services/LocationPermission';
+const height = Dimensions.get('window').height
+const width = Dimensions.get('window').width
+const ASPECT_RATIO = width / height;
+const LATITUDE = -33.3018708;
+const LONGITUDE = -66.3298548;
+const LATITUDE_DELTA = 0.00422;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+export default class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+            },
+        }
+    }
+    onRegionChange = region => {
+        this.setState({
+            region
+        })
+    }
+    componentDidMount = () => {
+        hasLocationPermission()
+        this._getLocation()
+    }
+    _getLocation = async () => {
+        await Geolocation.getCurrentPosition(
+            async posicion => {
+                const longitude = posicion.coords.longitude;
+                const latitude = posicion.coords.latitude;
+                this.mapRef.animateToRegion(
+                    {
+                        latitude,
+                        longitude,
+                        latitudeDelta: this.state.region.latitudeDelta,
+                        longitudeDelta: this.state.region.longitudeDelta
+                    },
+                    1000
+                );
+                this.setState({ region: { ...this.state.region, longitude, latitude } })
+                console.log('posicion actual... Latitud: ' + `${JSON.stringify(longitude)}` + 'Latitud: ' + `${JSON.stringify(latitude)}`)
+            },
+            (error) => {
+                console.log('')
+                console.log('')
+                console.log('')
+                console.log('')
+                console.log(error.code, error.message);
+            },
+            {
+                accuracy: {
+                    android: 'high',
+                    ios: 'best',
+                },
+                enableHighAccuracy: true,
+                timeout: 15000,
+                maximumAge: 10000,
+                distanceFilter: 0,
+                forceRequestLocation: true,
+            }
+        )
+    }
+    async fitCoordinates() {
+        console.log('centrando mapa')
+        this._getLocation()
+    }
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <MapView
+                    ref={map => {
+                        this.mapRef = map;
+                    }}
+                    mapType='standard'
+                    style={styles.map}
+                    initialRegion={this.state.region}
+                    // region={this.state.region}
+                    onRegionChangeComplete={this.onRegionChange}
+                />
+                <View style={{
+                    position: 'absolute', flexDirection: 'row',
+                    backgroundColor: 'white', borderRadius: 100, width: width / 10, alignSelf: 'flex-end',
+                    margin: 20, marginRight: 30, alignItems: 'center', justifyContent: 'center'
+                }}>
+                    <Icon
+                        name="crosshairs"
+                        type="font-awesome"
+                        color='#8d2d84'
+                        size={width / 10}
+                        onPress={() => this.fitCoordinates()}
+                    />
+                </View>
+                <View style={styles.markerFixed}>
+                    <Image style={styles.marker} source={require('../assets/images/pin.png')}
+                    />
+                </View>
+                <SafeAreaView style={styles.footer}>
+                    <Text style={styles.region}>longitud:
+                        {JSON.stringify(this.state.region.longitude)}{"\n"}latitud:
+                        {JSON.stringify(this.state.region.latitude)}</Text>
+                </SafeAreaView>
             </View>
-        </ImageBackground>
-    </SafeAreaView>
-    );
+        )
+    }
 }
-
 const styles = StyleSheet.create({
     text: {
-      fontSize: 30,
-      fontWeight: 'bold',
-      color: '#fff',
-      textAlign: 'center'
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center'
     },
-    box: {
-      margin: width / 20,
-      height: width / 2.5,
-      width: width / 2.5,
-      borderRadius: 15,
-      justifyContent: 'center',
-      backgroundColor: 'rgba(28, 89, 96, .7)',
-      zIndex: 1
+    content: {
+        margin: width / 20,
+        height: width / 2.5,
+        width: width / 2.5,
+        borderRadius: 15,
+        justifyContent: 'center',
+    },
+    markerFixed: {
+        left: '50%',
+        marginLeft: -24,
+        marginTop: -48,
+        position: 'absolute',
+        top: '50%'
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        width,
+        height,
+        alignSelf: 'center'
+    },
+    marker: {
+        height: 48,
+        width: 48
+    },
+    footer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        bottom: 30,
+        position: 'absolute',
+        width: '100%'
+    },
+    region: {
+        color: '#fff',
+        lineHeight: 20,
+        margin: 20,
+        alignSelf: 'center'
     }
-});
- 
-export default Map;
+})
